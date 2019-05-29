041a3f1062589774f6292460dbeecc43570d4c05
Jay Sheth <jay@JayS.local>
2019-05-29 10:47:20 +0530

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Switch, Link,
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import MoviesList from './MoviesList';
+import MovieDetail from './MovieDetail';
 
-function App() {
-  return (
+const App = () => (
+  <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
       </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
     </div>
-  );
-}
-
+  </Router>
+);
 export default App;
---
+++ src/MoviesList.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import Movie from './Movies';
+
+class MoviesList extends PureComponent {
+  state = {
+    movies: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=7e33a8665b14e3c290b296c99cfa7418&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+      );
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <MovieGrid>
+        {this.state.movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </MovieGrid>
+    );
+  }
+}
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+display: grid;
+grid-template-columns: repeat(6, 1fr);
+grid-row-gap: 1rem;
+padding: 1rem;
+`;
